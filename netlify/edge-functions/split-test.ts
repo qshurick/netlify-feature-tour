f662df8bdb15e1ae18da4134ead631872051851c
Alexander Malyk <alexander.malyk@reaktor.com>
2025-01-24 10:32:27 +0100

    Add connections to sanity

--- netlify/edge-functions/split-test.ts
+++ netlify/edge-functions/split-test.ts
@@ -1,4 +1,6 @@
 import type {Context, Config} from '@netlify/edge-functions'
+import {createClient} from '@sanity/client'
+
 export default async (request : Request, context : Context) => {
   const url = new URL(request.url)
   if (url.searchParams.get('split')) {
@@ -12,6 +14,31 @@ export default async (request : Request, context : Context) => {
       return new URL(`/branch-3/${url.pathname}?split=true`, url.origin)
     }
   }
+
+  async function getExperiment() {
+    const client = createClient({
+      projectId: "g63ug4wz",
+      dataset: "production",
+      useCdn: true, // set to `false` to bypass the edge cache
+      apiVersion: "2023-05-03" // use current date (YYYY-MM-DD) to target the latest API version
+      // token: process.env.SANITY_SECRET_TOKEN // Needed for certain operations like updating content or accessing previewDrafts perspective
+    });
+
+    const experiments = await client.fetch('*[_type == "split-test"]');
+
+    console.log(experiments);
+
+    return {
+      isActive: experiments.length > 0,
+    }
+  }
+
+  const experiment = await getExperiment();
+
+  if (!experiment.isActive) {
+    return
+  }
+
   if (variantFromCookie) {
     return sendRewrite(variantFromCookie)
   } else {
